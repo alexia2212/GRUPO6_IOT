7d2d264fa6565e660ee6fcb72078be0230f55939
alexia2212 <a20192270@pucp.edu.pe>
2023-09-13 20:49:00 -0500

    login 2 vista

--- app/src/main/java/com/example/grupo_iot/Login.java
+++ app/src/main/java/com/example/grupo_iot/Login.java
@@ -3,12 +3,26 @@ package com.example.grupo_iot;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.example.grupo_iot.databinding.ActivityMainBinding;
 
 public class Login extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Button button = findViewById(R.id.iniciarSesion);
+        button.setOnClickListener(view -> {
+
+                }
+
+                );
     }
 }
