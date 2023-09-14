9abad2cd76efabcd8c8a393b28b01cc7bbbdf05a
Jhondpc <a20196137@pucp.edu.pe>
2023-09-14 07:09:29 -0500

    sidebar avanzado

--- app/src/main/java/com/example/grupo_iot/alumno/MenuEventosActivity.java
+++ app/src/main/java/com/example/grupo_iot/alumno/MenuEventosActivity.java
@@ -1,23 +1,51 @@
 package com.example.grupo_iot.alumno;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.example.grupo_iot.R;
 
 public class MenuEventosActivity extends AppCompatActivity {
 
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_eventos);
-    }
 
+        ImageView abrirSidebar = findViewById(R.id.imageView6);
+        //ImageView cerrarSidebar = findViewById(R.id.cerrarSidebar);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        abrirSidebar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                } else {
+                    drawerLayout.openDrawer(GravityCompat.START);
+                }
+            }
+        });
+        /*
+        cerrarSidebar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Cierra el sidebar al hacer clic en el botón "Cerrar Sidebar"
+                drawerLayout.closeDrawer(GravityCompat.START);
+            }
+        });*/
+    }
     public void irEvento(View view){
         Intent intent = new Intent(this, EventoActivity.class);
         startActivity(intent);
     }
+
 }
