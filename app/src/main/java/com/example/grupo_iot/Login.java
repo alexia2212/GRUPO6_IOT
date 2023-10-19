6b0d24d7cbd2c6c960edf178d43d2e0bd5ebb34c
Jhondpc <a20196137@pucp.edu.pe>
2023-10-18 22:54:57 -0500

    usando firebase

--- app/src/main/java/com/example/grupo_iot/Login.java
+++ app/src/main/java/com/example/grupo_iot/Login.java
@@ -5,17 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 
+import com.example.grupo_iot.databinding.ActivityLoginBinding;
 import com.example.grupo_iot.databinding.ActivityMainBinding;
 
 public class Login extends AppCompatActivity {
-    ActivityMainBinding binding;
+    ActivityLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        //setContentView(R.layout.activity_login);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
 
