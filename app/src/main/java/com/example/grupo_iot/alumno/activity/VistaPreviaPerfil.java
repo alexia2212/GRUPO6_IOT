6be81e0c36a3b18b58b93633b28a80f78c32ab0f
alexia2212 <a20192270@pucp.edu.pe>
2023-11-01 17:06:10 -0500

    vista previa mi perfil

---
+++ app/src/main/java/com/example/grupo_iot/alumno/activity/VistaPreviaPerfil.java
@@ -0,0 +1,16 @@
+package com.example.grupo_iot.alumno.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.grupo_iot.R;
+
+public class VistaPreviaPerfil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vista_previa_perfil);
+    }
+}
