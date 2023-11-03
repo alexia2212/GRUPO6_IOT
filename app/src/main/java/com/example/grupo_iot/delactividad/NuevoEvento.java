56e56495d333dc304117f84f6c86847f9b0feb01
angie2512 <a20191792@pucp.edu.pe>
2023-11-03 14:38:09 -0500

    avancecrear

--- app/src/main/java/com/example/grupo_iot/delactividad/NuevoEvento.java
+++ app/src/main/java/com/example/grupo_iot/delactividad/NuevoEvento.java
@@ -9,6 +9,9 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.grupo_iot.LoginActivity;
@@ -17,6 +20,9 @@ import com.example.grupo_iot.databinding.ActivityActualizarBinding;
 import com.example.grupo_iot.databinding.ActivityNuevoEventoBinding;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -25,7 +31,10 @@ import java.util.List;
 
 public class NuevoEvento extends AppCompatActivity {
 
+    private TextInputEditText etFecha;
+
     ActivityNuevoEventoBinding binding;
+    private Spinner spinnerLugar;
     FirebaseFirestore db;
 
     @Override
@@ -42,6 +51,40 @@ public class NuevoEvento extends AppCompatActivity {
             }
         });
 
+        spinnerLugar = findViewById(R.id.lugarnuevoevento);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.simple_items, // El array de opciones definido en strings.xml
+                android.R.layout.simple_spinner_item // Diseño predeterminado para el Spinner
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinnerLugar.setAdapter(adapter);
+
+        spinnerLugar.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                // Obtén la opción seleccionada por el usuario
+                String selectedLugar = spinnerLugar.getSelectedItem().toString();
+                // Puedes hacer lo que necesites con la opción seleccionada, como almacenarla en una variable
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // En caso de que no se seleccione nada
+            }
+        });
+
+        etFecha = findViewById(R.id.fechanuevoevento);
+        etFecha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePicker(); // Llama a la función para mostrar el selector de fecha
+            }
+        });
+
+
     }
 
     private void guardarNuevoEvento() {
@@ -49,29 +92,44 @@ public class NuevoEvento extends AppCompatActivity {
         String titulo = binding.titulonuevoevento.getText().toString();
         String descripcion = binding.descripcionuevoevento.getText().toString();
         String fecha = binding.fechanuevoevento.getText().toString();
-        String lugar = binding.lugarnuevoevento.getText().toString();
+        String lugar = spinnerLugar.getSelectedItem().toString();
+
         String foto = binding.fotonuevoevento.getText().toString();
 
-        // Crea un nuevo objeto para representar el evento
         EventoList evento = new EventoList(titulo, fecha, foto, descripcion, lugar);
 
-        // Accede a la colección "listaeventos" y agrega un nuevo documento con los datos del evento
         db.collection("listaeventos")
                 .add(evento)
                 .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
                     @Override
                     public void onSuccess(DocumentReference documentReference) {
-                        // El evento se ha guardado con éxito
                         Toast.makeText(NuevoEvento.this, "Evento guardado con éxito", Toast.LENGTH_SHORT).show();
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        // Hubo un error al guardar el evento
                         Toast.makeText(NuevoEvento.this, "Error al guardar el evento", Toast.LENGTH_SHORT).show();
                     }
                 });
+
+
+    }
+
+    private void showDatePicker() {
+        MaterialDatePicker<Long> datePicker =
+                MaterialDatePicker.Builder.datePicker()
+                        .setTitleText("Select date")
+                        .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                        .build();
+        datePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener<Long>() {
+            @Override
+            public void onPositiveButtonClick(Long selection) {
+                // Actualiza el contenido del TextInputEditText de fecha con la fecha seleccionada
+                etFecha.setText(datePicker.getHeaderText());
+            }
+        });
+        datePicker.show(getSupportFragmentManager(), "DATE_PICKER_TAG");
     }
 
 
