c5c803f15586364da87ddd16c4a3e73d610a2987
alexia2212 <a20192270@pucp.edu.pe>
2023-12-03 15:12:53 -0500

    punteros en el mapa

--- app/src/main/java/com/example/grupo_iot/alumno/activity/MapsActivity.java
+++ app/src/main/java/com/example/grupo_iot/alumno/activity/MapsActivity.java
@@ -1,13 +1,19 @@
 package com.example.grupo_iot.alumno.activity;
+
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
+
 import android.Manifest;
+
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.grupo_iot.R;
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -16,11 +22,16 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
     private GoogleMap mMap;
     private FusedLocationProviderClient fusedLocationClient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,41 +44,62 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
 
 
-
-        // Verifica y solicita permisos si es necesario
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
-                    LOCATION_PERMISSION_REQUEST_CODE);
-        }
     }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(false);
 
-        // Muestra la ubicación actual en el mapa
-        mostrarUbicacionActual();
+        Antut(googleMap);
+        //mostrarUbicacionActual();
     }
 
 
+    public void Antut(GoogleMap googleMap){
+        mMap = googleMap;
+        final LatLng polideportivo = new LatLng(-12.066563783051707, -77.08034188931863);
+        final LatLng pabellonv = new LatLng(-12.073052142865027,-77.08198769436298);
+        final LatLng canchaminas = new LatLng(-12.07218069313465,-77.08193290503175);
+        final LatLng estacionamiento = new LatLng(-12.066945782900067,-77.08138386196286);
+        final LatLng digimundo = new LatLng(-12.073168701621984,-77.0811371912099);
+
+        mMap.addMarker(new MarkerOptions().position(polideportivo).title("Polideportivo").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+        mMap.addMarker(new MarkerOptions().position(pabellonv).title("Pabellon V").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE)));
+        mMap.addMarker(new MarkerOptions().position(canchaminas).title("Cancha Minas").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+        mMap.addMarker(new MarkerOptions().position(estacionamiento).title("Estacionamiento M").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+        mMap.addMarker(new MarkerOptions().position(digimundo).title("Digimundo").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA)));
+
+
+    }
     private void mostrarUbicacionActual() {
         try {
-            // Obtiene la última ubicación conocida
-            fusedLocationClient.getLastLocation()
-                    .addOnSuccessListener(this, location -> {
-                        if (location != null) {
-                            LatLng currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
-                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 15f));
-                        }
-                    });
+            // Verifica si los servicios de ubicación están habilitados
+            LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            if (locationManager != null && locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+
+                // Obtiene la última ubicación conocida
+                fusedLocationClient.getLastLocation()
+                        .addOnSuccessListener(this, location -> {
+                            if (location != null) {
+                                LatLng currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 15f));
+                            }
+                        });
+            } else {
+                // Si los servicios de ubicación no están habilitados, muestra un mensaje al usuario
+                Toast.makeText(this, "Habilita los servicios de ubicación", Toast.LENGTH_SHORT).show();
+            }
         } catch (SecurityException e) {
             e.printStackTrace();
         }
     }
-    // Maneja la respuesta de la solicitud de permisos
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
