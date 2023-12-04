5038cdc7860efa2ad4a6f2cd41461e51cde57287
alexia2212 <a20192270@pucp.edu.pe>
2023-12-04 15:00:40 -0500

    posicion del mapa

--- app/src/main/java/com/example/grupo_iot/alumno/activity/MapsActivity.java
+++ app/src/main/java/com/example/grupo_iot/alumno/activity/MapsActivity.java
@@ -10,12 +10,15 @@ import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.example.grupo_iot.R;
+import com.example.grupo_iot.alumno.entity.Alumno;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -26,10 +29,12 @@ import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
     private GoogleMap mMap;
+    Alumno alumnoIngresado;
     private FusedLocationProviderClient fusedLocationClient;
 
     @Override
@@ -37,12 +42,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        Intent intent = getIntent();
+        alumnoIngresado = (Alumno) intent.getSerializableExtra("alumno");
 
         // Obtén el mapa de Google Maps
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
+        generarBottomNavigationMenu();
 
     }
 
@@ -66,13 +73,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         final LatLng polideportivo = new LatLng(-12.066563783051707, -77.08034188931863);
         final LatLng pabellonv = new LatLng(-12.073052142865027,-77.08198769436298);
         final LatLng canchaminas = new LatLng(-12.07218069313465,-77.08193290503175);
-        final LatLng estacionamiento = new LatLng(-12.066945782900067,-77.08138386196286);
+        final LatLng bati = new LatLng(-12.073253462961755, -77.08163320224719);
         final LatLng digimundo = new LatLng(-12.073168701621984,-77.0811371912099);
 
         mMap.addMarker(new MarkerOptions().position(polideportivo).title("Polideportivo").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
         mMap.addMarker(new MarkerOptions().position(pabellonv).title("Pabellon V").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE)));
         mMap.addMarker(new MarkerOptions().position(canchaminas).title("Cancha Minas").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
-        mMap.addMarker(new MarkerOptions().position(estacionamiento).title("Estacionamiento M").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+        mMap.addMarker(new MarkerOptions().position(bati).title("Bati").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
         mMap.addMarker(new MarkerOptions().position(digimundo).title("Digimundo").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA)));
 
 
@@ -111,5 +118,38 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         }
     }
+    public void generarBottomNavigationMenu(){
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                if(menuItem.getItemId()==R.id.navigation_lista_actividades){
+                    Intent intent = new Intent(MapsActivity.this, ListaActividadesActivity.class);
+                    intent.putExtra("alumno", alumnoIngresado);
+                    startActivity(intent);
+                }
+                if(menuItem.getItemId()==R.id.navigation_eventos_apoyados){
+                    Intent intent = new Intent(MapsActivity.this, ListaEventosApoyadosActivity.class);
+                    intent.putExtra("alumno", alumnoIngresado);
+                    startActivity(intent);
+                }
+                if(menuItem.getItemId()==R.id.navigation_lista_chats){
+                    Intent intent = new Intent(MapsActivity.this, ListaDeChatsActivity.class);
+                    intent.putExtra("alumno", alumnoIngresado);
+                    startActivity(intent);
+                }
+                if(menuItem.getItemId()==R.id.navigation_donaciones){
+                    Intent intent = new Intent(MapsActivity.this, DonacionesActivity.class);
+                    intent.putExtra("alumno", alumnoIngresado);
+                    startActivity(intent);
+                }
+                if(menuItem.getItemId()==R.id.navigation_perfil){
+                    menuItem.setEnabled(false);
+                    menuItem.setChecked(true);
+                }
+                return true;
+            }
+        });
+    }
 }
