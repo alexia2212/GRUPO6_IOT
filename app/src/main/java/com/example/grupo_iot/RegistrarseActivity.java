270175ac9174e14dbbf1f66f3bc47278e4ca3481
alexia2212 <a20192270@pucp.edu.pe>
2023-12-09 12:39:27 -0500

    registra actividad designada

--- app/src/main/java/com/example/grupo_iot/RegistrarseActivity.java
+++ app/src/main/java/com/example/grupo_iot/RegistrarseActivity.java
@@ -58,7 +58,6 @@ public class RegistrarseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityRegistroBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
         db = FirebaseFirestore.getInstance();
 
         //SECCION CONDICION USUARIO
