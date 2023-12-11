137fd9959e22cba16db51f3ce93563bddb1a0044
angie2512 <a20191792@pucp.edu.pe>
2023-12-10 19:23:15 -0500

    crud evento revisado

--- app/src/main/java/com/example/grupo_iot/delactividad/Perfildelact.java
+++ app/src/main/java/com/example/grupo_iot/delactividad/Perfildelact.java
@@ -79,6 +79,8 @@ public class Perfildelact extends AppCompatActivity {
             Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
             startActivityForResult(intent, PICK_IMAGE_REQUEST);
         });
+
+
     }
 
     private void fetchUserData() {
